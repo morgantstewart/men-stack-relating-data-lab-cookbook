93c16850d742ed19bb926aabba8b1683fe96dc42
morgantstewart <morgan@paysigil.com>
2025-07-23 15:56:24 -0400

    established setup

--- models/user.js
+++ models/user.js
@@ -14,3 +14,22 @@ const userSchema = mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
+
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  pantry: {
+    type: String,
+    required: true,
+  }
+});
+
+
+
--- server.js
+++ server.js
@@ -8,8 +8,15 @@ const morgan = require('morgan');
 const session = require('express-session');
 
 const authController = require('./controllers/auth.js');
+const foodsController = require('./controllers/foods.js');
+
 
 const port = process.env.PORT ? process.env.PORT : '3000';
+
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
+
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -42,7 +49,13 @@ app.get('/vip-lounge', (req, res) => {
   }
 });
 
+
+app.use(passUserToView);
 app.use('/auth', authController);
+app.use(isSignedIn);
+app.use('/users/:userId/foods', foodsController);
+
+
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
